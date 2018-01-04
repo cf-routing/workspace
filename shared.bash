940d0308577ae2a28f178c2c5525146bd5cd2434
Aaron Hurley <ahurley@pivotal.io>
2018-01-04 10:23:06 -0800

    git colors has now changed its install
    Signed-off-by: Zachary Gershman <zgershman@pivotal.io>

--- shared.bash
+++ shared.bash
@@ -83,6 +83,7 @@ function main() {
       export GIT_PROMPT_ONLY_IN_REPO=0
       export GIT_PROMPT_THEME="Custom"
 
+      __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
       source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
     fi
   }
