de46079343eae6fc00c3f66d43f4554260203653
Zachary Gershman <zgershman@pivotal.io>
2018-05-29 08:50:25 -0700

    you may have more than one docker image

--- shared.bash
+++ shared.bash
@@ -213,7 +213,7 @@ istio_docker() {
     docker pull istio/ci:"${tag}"
 
     local image_id
-    image_id=$(docker images -f reference=istio/ci --format "{{.ID}}")
+    image_id=$(docker images -f reference=istio/ci --format "{{.ID}}" | head -n1)
 
     docker run -it --cap-add=NET_ADMIN -v "${istio_dir}":/go/src/istio.io/istio "${image_id}" /bin/bash
   else
