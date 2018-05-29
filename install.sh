9a8b6034c850e65697ac8abcf1c7c1998c387179
Utako Ueda <uueda@pivotal.io>
2018-05-29 14:41:52 -0700

    add docs-books-cloudfoundry
    Co-authored-by: Nancy Hsieh <nhsieh@pivotal.io>

--- install.sh
+++ install.sh
@@ -233,6 +233,7 @@ all_the_repos() {
   clone_if_not_exist "https://github.com/cloudfoundry/routing-perf-release" "${HOME}/workspace/routing-perf-release"
   clone_if_not_exist "https://github.com/cloudfoundry/routing-release" "${HOME}/workspace/routing-release"
   clone_if_not_exist "https://github.com/cloudfoundry/routing-sample-apps" "${HOME}/workspace/routing-sample-apps"
+  clone_if_not_exist "https://github.com/cloudfoundry/docs-book-cloudfoundry" "${HOME}/workspace/docs-book-cloudfoundry"
 }
 
 main "$@"
